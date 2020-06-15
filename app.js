14286e81155264073e18d9fdf33ebace8b5eef78
Du Luong <luongvidu@gmail.com>
2020-06-15 10:34:33 +1000

    Routing request

--- app.js
+++ app.js
@@ -6,14 +6,22 @@ const http = require('http');
 
 //res.end : ends the response crafting process and at this point send back response to user
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    if (url === "/"){
+        res.write("<html>");
+        res.write("<header><title>Message</title></header>");
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>");
+        res.write("</html>");
+        //return res.end so it wont execute following code after res.end
+        return res.end();
+    }
     res.setHeader('Content-Type','text/html');
     res.write("<html>");
     res.write("<header><title>Hello world</title></header>");
-    res.write("<body></body>");
+    res.write("<body><h1>My server</h1></body>");
     res.write("</html>");
     res.end();
-
+    //process.exit()
 })
 
 //server not executing the script immediately but listen to request
